3bf9e7cc57817a8980eb5fc74a50dc307d415e33
ki <tsutsura18@gmail.com>
2024-07-22 01:26:27 +0300

    edited and added files to get a response when working with API

--- data/response/web_response.go
+++ data/response/web_response.go
@@ -1,7 +1,8 @@
 package response
 
 type Response struct {
-	Code   int         `json:"code"`
-	Status string      `json:"status"`
-	Data   interface{} `json:"data,omitempty"`
+	Code    int         `json:"code"`
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
