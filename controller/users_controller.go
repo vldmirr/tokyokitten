7634425b8b7ecb72cfba16ddb4c6e3a0563fc2ae
ki <tsutsura18@gmail.com>
2024-07-22 01:15:03 +0300

    created controllers to check the operation of login, registration and issuance of user data

---
+++ controller/users_controller.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"tokyokitten/data/response"
+	"tokyokitten/repository"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	userRepository repository.UsersRepository
+}
+
+func NewUsersController(repository repository.UsersRepository) *UserController {
+	return &UserController{userRepository: repository}
+}
+
+func (controller *UserController) GetUsers(ctx *gin.Context) {
+	// currentUser := ctx.MustGet("currentUser").(model.Users)
+	users := controller.userRepository.FindAll()
+	webResponse := response.Response{
+		Code:    200,
+		Status:  "Ok",
+		Message: "Successfully fetch all user data!",
+		Data:    users,
+	}
+
+	ctx.JSON(http.StatusOK, webResponse)
+}
